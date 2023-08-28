c46dbc70502bf87122e92843171f50d99d707dcf
neelimapriya <neelimasultana6@gmail.com>
2023-08-29 01:18:07 +0600

    setup search field and show result

---
+++ js/phone.js
@@ -0,0 +1,46 @@
+const loadPhone= async(searchText)=>{
+    const res =await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+    const data =await res.json();
+    const phones =data.data;
+    // console.log(phones);
+    displayPhones(phones)
+}
+const displayPhones =phones =>{
+    // console.log(phones)
+
+    const phoneContainer =document.getElementById('phone-container')
+    // clear phone container cards before adding new
+    phoneContainer.innerText='';
+
+    phones.forEach(phones => {
+        // console.log(phones);
+        // 2. create a div
+        const phoneCard = document.createElement('div')
+        phoneCard.classList=`card bg-gray-200 p-4
+         bg-base-100 shadow-xl`;
+        // 3. set innerHtml
+        phoneCard.innerHTML=`
+        <figure><img src="${phones.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phones.phone_name }</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>`;
+        // 4.append child
+        phoneContainer.appendChild(phoneCard)
+    })
+}
+
+// handle search button
+const handleSearch=()=>{
+    const searchField =document.getElementById('search-field');
+    const searchText =searchField.value;
+    console.log(searchText)
+    loadPhone(searchText);
+}
+
+
+
+
