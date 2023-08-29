42b7b3098089fb250d401f1d072f55da1bd5cda8
neelimapriya <neelimasultana6@gmail.com>
2023-08-29 11:44:42 +0600

    setup loading spinner

--- js/phone.js
+++ js/phone.js
@@ -10,7 +10,18 @@ const displayPhones =phones =>{
 
     const phoneContainer =document.getElementById('phone-container')
     // clear phone container cards before adding new
-    phoneContainer.innerText='';
+    phoneContainer.textContent='';
+      // display show all button if there more then 12 button
+      const showAllContainer =document.getElementById('show-all-container')
+      if(phones.length>12){
+        showAllContainer.classList.remove('hidden')
+      }
+      else{
+        showAllContainer.classList.add('hidden')
+      }
+
+    // display only first 12 phones
+    phones =phones.slice(0,12)
 
     phones.forEach(phones => {
         // console.log(phones);
@@ -30,15 +41,29 @@ const displayPhones =phones =>{
         </div>`;
         // 4.append child
         phoneContainer.appendChild(phoneCard)
-    })
+    });
+    // hide loading spinner
+    toggleLoadingSpinner(false);
 }
 
 // handle search button
 const handleSearch=()=>{
+  toggleLoadingSpinner(true);
     const searchField =document.getElementById('search-field');
     const searchText =searchField.value;
     console.log(searchText)
     loadPhone(searchText);
+}
+
+
+const toggleLoadingSpinner=(isLoading)=>{
+  const loadingSpinner =document.getElementById('loading-spinner');
+  if(isLoading){
+    loadingSpinner.classList.remove('hidden')
+  }
+  else{
+    loadingSpinner.classList.add('hidden')
+  }
 }
 
 
